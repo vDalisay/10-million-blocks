9b98bde95d1fac704c4039d62d6a6249ab1fae4c
vDalisay <andersenvincentr@gmail.com>
2026-08-19 22:18:12 +0200

    Remove no-longer-needed voxel dot helper

--- src/Automation/VoxelVectorExtensions.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-namespace TenMillionBlocks.Automation;
-
-internal static class VoxelVectorExtensions
-{
-    public static int Dot(this Vector3I value, Vector3I other)
-        => value.X * other.X + value.Y * other.Y + value.Z * other.Z;
-}
