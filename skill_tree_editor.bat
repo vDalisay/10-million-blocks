b1009ba83b3cd3739e2407673c1090f0870d9d14
vDalisay <andersenvincentr@gmail.com>
2026-08-19 15:55:16 +0200

    Fix skill tree editor launcher

--- skill_tree_editor.bat
+++ skill_tree_editor.bat
@@ -12,7 +12,7 @@ set "GODOT_GUI_EXE=%GODOT_EXE:_console.exe=.exe%"
 if /I not "%GODOT_GUI_EXE%"=="%GODOT_EXE%" if exist "%GODOT_GUI_EXE%" set "GODOT_EXE=%GODOT_GUI_EXE%"
 
 pushd "%PROJECT_ROOT%"
-"%GODOT_EXE%" --path "%PROJECT_ROOT%" "res://tools/skill_tree_editor/SkillTreeEditor.tscn"
+"%GODOT_EXE%" --path . "res://tools/skill_tree_editor/SkillTreeEditor.tscn"
 set "RESULT=%ERRORLEVEL%"
 popd
 exit /b %RESULT%
