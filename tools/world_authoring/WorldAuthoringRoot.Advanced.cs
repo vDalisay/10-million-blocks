8436b73af69084e3e05f440e5df70458c5e8e7e9
vDalisay <andersenvincentr@gmail.com>
2026-08-21 11:30:32 +0200

    Fix advanced authoring profile namespace

--- tools/world_authoring/WorldAuthoringRoot.Advanced.cs
+++ tools/world_authoring/WorldAuthoringRoot.Advanced.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Godot;
+using TenMillionBlocks.Content;
 using TenMillionBlocks.World.Authoring;
 using TenMillionBlocks.World.Rendering;
 
