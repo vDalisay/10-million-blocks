eb005cbee20e1a0546289b59be7111f6b9d0d9c5
vDalisay <andersenvincentr@gmail.com>
2026-08-21 20:12:22 +0200

    Wire pause menu into gameplay

---
+++ src/App/GameRoot.PauseMenu.cs
@@ -0,0 +1,49 @@
+using Godot;
+using TenMillionBlocks.Presentation;
+using TenMillionBlocks.UI;
+
+namespace TenMillionBlocks.App;
+
+public partial class GameRoot
+{
+    private PauseMenuView? _pauseMenu;
+
+    public override void _EnterTree()
+    {
+        GraphicsSettingsRuntime graphics = GraphicsSettingsRuntime.Ensure(GetTree());
+        _pauseMenu = new PauseMenuView { Name = "PauseMenuView" };
+        _pauseMenu.Initialize(graphics, CanOpenPauseMenu);
+        _pauseMenu.ReturnToMainMenuRequested += OnPauseReturnToMainMenuRequested;
+        AddChild(_pauseMenu);
+    }
+
+    private bool CanOpenPauseMenu()
+    {
+        if (!_sessionPersists || _world is null || _completionShown || _replayView is not null)
+        {
+            return false;
+        }
+        if (_worldBrowser?.IsOpen == true || _skillTree?.IsOpen == true)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private void OnPauseReturnToMainMenuRequested()
+    {
+        if (_sessionPersists && _world is not null)
+        {
+            CaptureCurrentSession();
+            TrySaveCurrentSession(captureFirst: false);
+        }
+
+        _pauseMenu?.Close();
+        GetTree().Paused = false;
+        Error result = GetTree().ChangeSceneToFile("res://scenes/Main.tscn");
+        if (result != Error.Ok)
+        {
+            GD.PushError($"Could not return to main menu ({result}).");
+        }
+    }
+}
